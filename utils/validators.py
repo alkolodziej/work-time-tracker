559765c868f1ce165e0f11f612af101bb47b9bea
GamePL7 <gamepl7777@gmail.com>
2025-06-21 15:17:18 +0200

    Dodanie narzędzi walidujących i funkcji pomocniczych

---
+++ utils/validators.py
@@ -0,0 +1,27 @@
+### utils/validators.py
+from datetime import datetime
+import functools
+
+def log_operation(func):
+    """Dekorator logujący operacje na wpisach."""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"[LOG] Wywołano: {func.__name__}")
+        return func(*args, **kwargs)
+    return wrapper
+
+def validate_date(date_str):
+    """Waliduj datę w formacie YYYY-MM-DD."""
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
+def validate_time(time_str):
+    """Waliduj czas w formacie HH:MM."""
+    try:
+        datetime.strptime(time_str, "%H:%M")
+        return True
+    except ValueError:
+        return False
